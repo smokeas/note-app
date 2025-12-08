7ad37af55e5214d4b9a53b9548a88e9055b92b3c
Sinitsa Artem <artemsss009@gmail.com>
2025-12-08 17:44:01 +0300

    v1f

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -4,13 +4,13 @@ import (
 	"net/http"
 )
 
-// StartServer запускает HTTP-сервер на указанном порту.
+// StartServer запускает HTTP-сервер на указанном порту
 // Оборачиваем ОБработчики в middleware для логирования/авторизации и т.д.
 func StartServer(port string) {
 	http.Handle("/register", LoggingMiddleware(http.HandlerFunc(registerHandler)))
 	http.Handle("/login", LoggingMiddleware(http.HandlerFunc(loginHandler)))
 
-	// Слушаем на всех интерфейсах (0.0.0.0) и на указанном порту.
+	// Слушаем на всех интерфейсах (0.0.0.0) и на указанном порту
 	// В реальном приложении лучше обработать ошибку:
 	//    if err := http.ListenAndServe("0.0.0.0:"+port, nil); err != nil { log.Fatal(err) }
 	_ = http.ListenAndServe("0.0.0.0:"+port, nil) //nil — список правил
